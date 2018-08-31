27669a2398eea7f1f930feaf7d8114acea53063a
Pranjal <pranjalrastogi1998@gmail.com>
2018-09-01 00:01:29 +0530

    graphs value generation

--- backend/plot/longilati.py
+++ backend/plot/longilati.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import json
 
-g1=pd.read_csv('plot/extra/newdelhi.csv')
+g1=pd.read_csv('extra/newdelhi.csv')
 g2=g1.iloc[:,3:]
 longi=[]
 lati=[]
@@ -26,15 +26,29 @@ def smallvalues(lat,lon):
         if small<reqd:
             reqd=small
             index=i
-    #reqdvalues(index)
+            
+            
     keys=g2.head(0)
-    keys=keys.columns.tolist()
+    keys=keys.columns.tolist() 
     jsonvalue=g2.iloc[[index]]
-    jsonvalue=jsonvalue.values.tolist()
+    site=g2['SITE_NAME']
+    jsonsite=jsonvalue['SITE_NAME']
+    str(jsonsite)
+    site=site.tolist()
+    #print(site)
+    jsonvalue1=jsonvalue.values.tolist()
     for i in range(0,6):
-        g1dict[keys[i]]= jsonvalue[0][i]
-    jsondumps=json.dumps(g1dict)
-    return(jsondumps)   
+        g1dict[keys[i]]= jsonvalue1[0][i]
+        jsondumps=json.dumps(g1dict)
+    print(jsonsite)
+    #print(jsonvalue['SITE_NAME'])
+    for j in range(0,289):
+        if jsonsite==site[j]:
+            jsonvalue=g2.iloc[[j]]
+            jsonvalue1=jsonvalue.values.tolist()
+            print(jsonvalue1)
+                
+#print(jsonvalue)
       
 smallvalues(7.009,32.0987)
 
