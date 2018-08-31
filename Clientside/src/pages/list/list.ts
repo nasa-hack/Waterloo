b6708f7c5a43c00252e8ea7fe8b76ee2ad71d6b1
Mayank Sharma <mayanksharma250199@gmail.com>
2018-08-31 16:53:18 +0530

    app done!!

--- Clientside/src/pages/list/list.ts
+++ Clientside/src/pages/list/list.ts
@@ -8,24 +8,35 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    this.items = [{
+    name:"Water Level Rise/Falls",
+    description:" The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.",
+    img:"https://healthitanalytics.com/images/site/features/_normal/ThinkstockPhotos-645261596.jpg"
+    },
+    {
+      name:"Population",
+      description:" The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.",
+      img:"https://healthitanalytics.com/images/site/features/_normal/ThinkstockPhotos-645261596.jpg"
+    },
+    {
+      name:"Population Density",
+      description:" The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.",
+      img:"https://healthitanalytics.com/images/site/features/_normal/ThinkstockPhotos-645261596.jpg"
+    },
+    {
+      name:"Precipitation Rate",
+      description:" The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.",
+      img:"https://healthitanalytics.com/images/site/features/_normal/ThinkstockPhotos-645261596.jpg"
+    },
+  ];
+
   }
 
   itemTapped(event, item) {
