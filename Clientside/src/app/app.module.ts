92c8fe39ce421f13a53acb403f1aaf8d0cc84bc9
Mayank Sharma <mayanksharma250199@gmail.com>
2018-08-31 22:19:31 +0530

    chart added

--- Clientside/src/app/app.module.ts
+++ Clientside/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { RainPage } from '../pages//rain/rain';
+import { PopulationPage } from '../pages//population/population';
+import { PopDenPage } from '../pages//pop-den/pop-den';
+import { WaterPage } from '../pages//water/water';
 
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -15,23 +19,38 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    WaterPage,
+    PopDenPage,
+    PopulationPage,
+    RainPage
+    
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp)
+  ],
+  exports: [
+    IonicModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    WaterPage,
+    PopDenPage,
+    PopulationPage,
+    RainPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class AppModule {}
